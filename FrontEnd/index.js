da109618389b18af016e043936e0cc0a34a7db7e
Rukia0124 <asahari02@gmail.com>
2023-01-04 16:39:00 +0100

    feat filters working

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,16 +1,18 @@
 const gallery = document.querySelector(".gallery");
 const filters = document.querySelector(".filters");
+
 let galleryData = [];
+let filteredGalleryData = [];
 
 async function fetchGallery() {
   await fetch("http://localhost:5678/api/works")
     .then((res) => res.json())
     .then((data) => (galleryData = data));
   filtersDisplay();
-  worksDisplay();
+  worksDisplay(galleryData);
 }
-function worksDisplay() {
-  gallery.innerHTML = galleryData
+function worksDisplay(data) {
+  gallery.innerHTML = data
     .map(
       (work) =>
         `
@@ -32,10 +34,27 @@ function filtersDisplay() {
   filters.innerHTML = filtersArray
     .map(
       (filter) => `
-    <button id="filter">${filter}</button>
+    <button class="filter">${filter}</button>
     `
     )
     .join("");
+  addFilterListener();
+}
+function addFilterListener() {
+  let filter = document.querySelectorAll(".filter");
+  for (let i = 0; i < filter.length; i++) {
+    filter[i].addEventListener("click", (e) => {
+      const selectedFilter = e.target.innerText;
+      const filteredGalleryData = galleryData.filter(
+        (work) => work.category.name === selectedFilter
+      );
+      if (selectedFilter === "Tous") {
+        worksDisplay(galleryData);
+      } else {
+        worksDisplay(filteredGalleryData);
+      }
+    });
+  }
 }
 
 window.addEventListener("load", (event) => {
