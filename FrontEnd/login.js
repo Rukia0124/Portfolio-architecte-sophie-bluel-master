8bc80b2580cfb3b9087749cf87dcee774988d58e
Rukia0124 <asahari02@gmail.com>
2023-01-09 00:16:03 +0100

    feat: handle success login
    send request to the api
    store token in cookies

---
+++ FrontEnd/login.js
@@ -0,0 +1,35 @@
+document
+  .getElementById("login-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    let myHeaders = new Headers({
+      accept: "application/json",
+      "Content-Type": "application/json",
+    });
+
+    let myBody = JSON.stringify({
+      email: email,
+      password: password,
+    });
+
+    fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: myHeaders,
+      body: myBody,
+    })
+      .then(function (response) {
+        if (response.ok) {
+          response.json().then(function (data) {
+            document.cookie = "access_token=" + data.token;
+          });
+        } else {
+          alert("Connexion échouée");
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  });
